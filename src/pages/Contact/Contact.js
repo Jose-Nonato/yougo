6f9812f5f5eec14642ada66ab178bdfe9c14126f
JoseJunior <beijallote@gmail.com>
2023-05-08 15:45:42 -0300

    feat: página de contatos

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,12 +1,71 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import { Container, Texts, Cards, Forms, Images } from "./styles";
+import { ShareLocation, Phone, Email } from '@styled-icons/material';
+import Aeroporto from "./assets/Aeroporto.png";
+import Belem from "./assets/Belem.png";
 
 export default function Contact() {
     return(
-        <div>
+        <Container>
             <Header />
+
+                <Texts>
+                    <h1>Entre em Contato</h1>
+                    <p>Tire suas dúvidas com nossa equipe especializada em tornar o seu atendimento rápido e prático.</p>
+                </Texts>
+
+                <Cards>
+                    <div className="card">
+                        <ShareLocation className="icon"/>
+                        <p>Rua Pequena 102</p>
+                    </div>
+                    <div className="card">
+                        <Phone className="icon"/>
+                        <p>(91) 99999-9999</p>
+                    </div>
+                    <div className="card">
+                        <Email className="icon"/>
+                        <p>yougo@gmail.com</p>
+                    </div>
+                </Cards>
+
+                <Forms>
+                    <div className="card">
+                        <h2>Formulário de Contato</h2>
+                        <div className="info">
+                            <input type="text" placeholder="Seu Nome ..."/>
+                            <input type="text" placeholder="Seu Email ..."/>
+                        </div>
+                        <div className="textarea">
+                            <textarea id="w3review" name="w3review" rows="4" cols="90"></textarea><br/>
+                        </div>
+                        <button>ENVIAR</button>
+                    </div>
+                </Forms>
+
+                <Images>
+                    <h1>Onde nos encontrar ?</h1>
+                    <div className="teste">
+                        <div className="text">
+                            <img src={Aeroporto} alt="Localização Aeroporto"/>
+                            <div>
+                                <h3>Aeroporto Santos Dumont Rio de Janeiro</h3>
+                                <p>Stand 1 - térreo do aeroporto, ao lado do check in da azul linhas aéreas.<br/>Stand 2 - corredor do aeroporto no segundo andar próximo a entrada do embarque doméstico.</p>
+                            </div>
+                        </div>
+                        <div className="text">
+                            <img src={Belem} alt="Localização Belem"/>
+                            <div>
+                                <h3>Aeroporto de Belém</h3>
+                                <p>Stand 1 - área externa na frente do check in das companhias aéreas.<br/>Stand 2 - área restrita do aeroporto, ao lado do portão de embarque 4.</p>
+                            </div>
+                        </div>
+                    </div>
+                </Images>
+
             <Footer />
-        </div>
+        </Container>
     );
 };
---
+++ src/pages/Contact/styles.js
@@ -0,0 +1,121 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    color: #606060;
+`;
+
+export const Texts = styled.div`
+    padding: 20px;
+
+    h1 {
+        padding: 10px 0px;
+    }
+
+    h1::after {
+        content: "";
+        width: 50px;
+        height: 5px;
+        background-color: #CA60C6;
+        border-radius: 10px;
+        display: block;
+    }
+
+    p {
+        padding-bottom: 5px;
+    }
+`;
+
+export const Cards = styled.div`
+    display: flex;
+    justify-content: space-around;
+    text-align: center;
+    padding: 20px 5px;
+
+    .card {
+        padding: 10px;
+        border-radius: 10px;
+        transition: .3s ease-in-out;
+
+        &:hover {
+            background-color: #28CA50;
+            color: #FFF;
+        }
+    }
+
+    .icon {
+        width: 100px;
+        height: 100px;
+    }
+`;
+
+export const Forms = styled.div`
+    padding: 5px 0px 40px 0px;
+
+    .card {
+        background-color: #F5F5F5;
+        width: 70%;
+        border-radius: 20px;
+        padding: 20px 40px;
+        box-shadow: 4px 4px 4px rgba(0,0,0,.18);
+        margin: 0 auto;
+
+        h2 {
+            padding: 10px 0;
+        }
+
+        .info {
+            display: flex;
+            justify-content: space-evenly;
+            
+            input {
+                width: 40%;
+                padding: 10px 15px;
+                outline: none;
+                margin: 15px;
+            }
+        }
+
+        .textarea {
+            display: flex;
+            justify-content: center;
+
+            textarea {
+                outline: none;
+                width: 90%;
+            }
+        }
+
+        button {
+            width: 30%;
+            margin-top: 20px;
+            border: none;
+            background-color: #28CA50;
+            padding: 10px 20px;
+            color: #F5F5F5;
+            font-weight: 600;
+            font-size: 16px;
+            border-radius: 10px;
+        }
+    }
+`;
+
+export const Images = styled.div`
+    padding: 20px;
+
+    h1 {
+        padding: 20px 0px;
+        &::after {
+            content: "";
+            width: 60px;
+            height: 5px;
+            background-color: #CA60C6;
+            border-radius: 10px;
+            display: block;
+        }
+    }
+
+    .teste {
+        display: flex;
+        justify-content: space-evenly;
+    }
+`;
